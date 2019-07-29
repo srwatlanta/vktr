190ed3604b2d733939a9556c2ab052101e5550a5
srwatlanta <shawnrwheeler@gmail.com>
2019-07-29 16:35:06 -0500

    views pages and controller functions

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -1,2 +1,7 @@
 class GenresController < ApplicationController
+  def index
+  end
+
+  def show
+  end
 end
--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,16 @@
 class ReviewsController < ApplicationController
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
 end
