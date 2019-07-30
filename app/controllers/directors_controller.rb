d6ca29ebe707b19a8ca0d09d839ba38739cdbf75
christianalim <christianalim1@gmail.com>
2019-07-30 09:35:53 -0500

    Added barebones views pages to artists, genres, and directors

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -1,7 +1,15 @@
 class DirectorsController < ApplicationController
   def index
+    @directors = Director.all
   end
 
   def show
+    find_director
+  end
+
+  private
+
+  def find_director
+    @director = Director.find_by(id: params[:id])
   end
 end
