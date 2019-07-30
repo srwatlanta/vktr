5b128ec5b2111ae5464293e4fbf9e91e57b32653
srwatlanta <shawnrwheeler@gmail.com>
2019-07-30 14:32:43 -0500

    session data

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,25 @@
 class SessionsController < ApplicationController
   def new
+    if logged_in?
+      redirect_to current_user
+    end
   end
 
   def create
+    username = params[:user][:username]
+    password = params[:user][:password]
+    user = User.find_by(username: username)
+    if user && user.authenticate(password)
+      session[:user_id] = user.id
+      redirect_to user
+    else
+      flash[:errors] = ["invalid credentials"]
+      redirect_to login_path
+    end
+  end
+
+  def destroy
+    session.delete :user_id
+    redirect_to login_path
   end
 end
