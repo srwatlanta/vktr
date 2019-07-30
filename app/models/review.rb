d0c3eb72617755522ea406b25329d9307cd092da
github username <github email address>
2019-07-30 17:47:59 -0500

    more validations

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,6 +2,6 @@ class Review < ApplicationRecord
     belongs_to :video
     belongs_to :user
 
-    # validates :kill_rating, presence: true
-    # validates :content, presence: true
+    validates :kill_rating, presence: true
+    validates :content, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,12 @@ class User < ApplicationRecord
   has_many :reviews, dependent: :destroy
   has_many :videos, through: :reviews
   has_secure_password
-  validates_uniqueness_of :username, :password_digest, :email, :img_url
+  validates_uniqueness_of :username, :password, :email, :img_url
   # validates :username, presence: true
   # validates :password_digest, presence: true
   # validates :email, presence: true
   # validates :img_url, presence: true
-  validates :username, :password, :email, :img_url, :presence => {:message => "cannot be blank"}
+  validates :username, :password, :email, :presence => {:message => "cannot be blank"}
+  validates_length_of :password, :minimum => 7
 
 end
