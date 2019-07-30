e46c04f42e62fbfb598da5911ac77021d9ef582c
srwatlanta <shawnrwheeler@gmail.com>
2019-07-30 18:34:22 -0500

    add button to login page and help create user functionality.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -14,6 +14,7 @@ class SessionsController < ApplicationController
       session[:user_id] = user.id
       redirect_to user
     else
+      flash[:errors] = ["invalid credentials"]
       redirect_to login_path
     end
   end
