49c168c84b58cd7e2d92eb053858c97bd0aa36a8
srwatlanta <shawnrwheeler@gmail.com>
2019-07-30 09:52:21 -0500

    videos index and show completed

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,8 +1,13 @@
 class Video < ApplicationRecord
-    belongs_to :artist
-    belongs_to :director
-    belongs_to :genre
+  belongs_to :artist
+  belongs_to :director
+  belongs_to :genre
 
-    has_many :reviews
-    has_many :users, through: :reviews
+  has_many :reviews
+  has_many :users, through: :reviews
+
+  def video_url_id
+    x = self.video_url.split("=")
+    return x[1]
+  end
 end
