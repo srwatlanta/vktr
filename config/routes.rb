a83a4697190b5e80e9c6244419b39cc269b779cf
christianalim <christianalim1@gmail.com>
2019-08-01 09:04:56 -0500

    added profile button on nav

--- config/routes.rb
+++ config/routes.rb
@@ -8,10 +8,6 @@ Rails.application.routes.draw do
   get "/login", to: "sessions#new", as: "login"
   post "/login", to: "sessions#create"
   delete "/logout", to: "sessions#destroy"
-<<<<<<< HEAD
-  root 'videos#index' 
-=======
   root "home#index"
->>>>>>> 8671cc3afdd7735e6062ba74fe811649ea826e69
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
